d155bfe581523685d198052669e72ffd8433e7a7
raktimunreal4@gmail.com <raktimunreal4@gmail.com>
2026-03-22 18:49:07 +0530

    Define color variables

--- NumberToNumberName/numbername.py
+++ NumberToNumberName/numbername.py
@@ -3,3 +3,9 @@
 # 61893: Sixty One Thousand Eight Hundred Ninety Three
 
 __import__('os').system('cls')
+
+
+Y = "\033[38;2;255;200;0m"
+W = "\033[38;2;212;212;212;0m"
+B = "\033[38;2;108;180;238m;0m"
+
